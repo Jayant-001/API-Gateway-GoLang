6f81cd73c79cd62b1b0401b3ad450c7b1545e994
Jayant Gupta <jayant.gupta.dln@gmail.com>
2025-09-24 21:39:33 +0530

    Added middlewares

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,1 +1,15 @@
 package middleware
+
+import "net/http"
+
+func Auth(apiKey string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.Header.Get("X-API-KEY") != apiKey {
+				http.Error(w, "Forbidden", http.StatusForbidden)
+				return
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
+}
